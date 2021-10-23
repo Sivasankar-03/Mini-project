3c8c7da64eaa3503c0a5e81e94cb4fca9e84208a
Sivasankar-03 <sivasankarb.20cse@kongu.edu>
2021-10-23 10:37:14 +0530

    First Commit

---
+++ Details.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class studentdetails
+{public static void main(String args[]){
+Scanner S = new Scanner(System.in);
+System.out.println("Name :");
+String a = s.next();
+System.out.println(Rollno :");
+String b = s.next();
+System.out.println(Father name :");
+String c = s.next();
+System.out.println("Mother Name :);
+String d = s.next();
+System.out.println("Age :");
+int e = s.nextInt();
+System.out.println(Mobile Number :");
+int f = s.nextInt();
+System.out.println("Email :");
+String g = s.next();
+System.out.println(Blood Group :");
+int h = nextInt();
+System.out.println("Address :");
+string i = nextInt();
+System.out.println("Aadhar Number :");
+int j = nxtInt();
+}
+}
