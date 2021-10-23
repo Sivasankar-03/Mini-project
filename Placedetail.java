62cb7b6f67d51305c5cd6a18bf997b8520bcb811
Sivasankar-03 <sivasankarb.20cse@kongu.edu>
2021-10-23 10:58:40 +0530

    Placement Commit

---
+++ Placedetail.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class studentdetails
+{public static void main(String args[]){
+Scanner S = new Scanner(System.in);
+System.out.println("Student Name :");
+String a = s.next();
+System.out.println(Rollno :");
+String b = s.next();
+System.out.println(Mobile Number :");
+int c = s.nextInt();
+System.out.println("Personal Mail Id :");
+String d = s.next();
+System.out.println("Educational Mail Id :");
+String e = s.next();
+System.out.println("DaysScholar(1)/Hosteller(2) :");
+int f = s.nextInt();
+System.out.println("Number of Arrears :");
+int g = s.nextInt();
+System.out.println("Number of Arrears :");
+int h = s.nextInt();
+System.out.println("GPA (Upto 1 st Sem):");
+int i = s.nextInt();
+System.out.println("GPA (Upto 2 nd Sem):");
+int j = s.nextInt();
+System.out.println("GPA (Upto 3 rd Sem):");
+int k = s.nextInt();
+System.out.println("CGPA (Upto 3 rd Sem):");
+int l = s.nextInt();
+}
+}
