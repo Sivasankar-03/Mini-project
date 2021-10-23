7799202d64aecab65cd5ae4aaa1b1b1a82e26efa
Sivasankar-03 <sivasankarb.20cse@kongu.edu>
2021-10-23 10:46:37 +0530

    Insurance Commit

---
+++ Insdetail.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class insdetails
+{public static void main String args[]{
+Scanner S = new Scanner(System.in);
+System.out.println("Student Name :");
+String a = s.next();
+System.out.println("Nominee Name :");
+String b = s.next();
+System.out.println("Relationship of Nominee :");
+String c = s.next();
+System.out.println("Nominee Age :");
+int d = s.nextInt();
+System.out.println("Nominee Mobile Number :");
+int e = s.nextInt();
+System.out.println("Nominee Occupation :");
+String f = s.next();
+System.out.println("Nominee Email :");
+String g = s.next();
+System.out.println("Nominee Aadhaar Number :");
+String h = s.next();
+}
+}
