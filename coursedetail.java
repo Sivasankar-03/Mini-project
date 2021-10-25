e8feb06910658ff8f57187a9858dd33a5ecf3657
Sivasankar-03 <sivasankarb.20cse@kongu.edu>
2021-10-25 19:04:17 +0530

    Course Completed

---
+++ coursedetail.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class studentdetails
+{public static void main(String args[]){
+Scanner S = new Scanner(System.in);
+System.out.println("Roll no:");
+String a = s.next();
+System.out.println("Semester Number :");
+int b = s.nextInt();
+System.out.println("Roll no:");
+String c = s.next();
+System.out.println("No of One credit courses completed:");
+int d = s.nextInt();
+System.out.println("Name of One credit courses completed:");
+int arr[]=s.nextInt[];
+System.out.println("No of value added courses completed:");
+int e = s.nextInt();
+System.out.println("Name of value added courses:");
+String f[]=s.next[];
+}
+}
