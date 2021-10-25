b3ab6e4a88b4d57636803a946a09f01ae30fa567
Sivasankar-03 <sivasankarb.20cse@kongu.edu>
2021-10-25 18:57:51 +0530

    activity commit

---
+++ activitydetail.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class studentdetails
+{public static void main(String args[]){
+Scanner S = new Scanner(System.in);
+System.out.println("Roll No :");
+String a = s.next();
+System.out.println("Semester :");
+int b = s.nextInt();
+System.out.println("No. of papers presented inside KEC:");
+int c = s.nextInt();
+System.out.println("No. of projects presented inside KEC:");
+int d = s.nextInt();
+System.out.println("No. of papers presented outside KEC:");
+int e = s.nextInt();
+System.out.println("No. of projects presented outside KEC:");
+int f = s.nextInt();
+System.out.println("No. of prize won in Coding Carnival:");
+int g = s.nextInt();
+System.out.println("No. of prize won in Hackathon:");
+int h = s.nextInt();
+}
+}
